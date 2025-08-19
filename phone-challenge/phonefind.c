9eaaf9efb2132ca14892ce12695eb5a6cd50a21a
Pedro Costa <pmdacosta@protonmail.com>
2025-08-19 20:36:57 +0100

    phonefind complete

--- phone-challenge/phonefind.c
+++ phone-challenge/phonefind.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE // needed for strcasestr
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +6,12 @@
 #define INITIAL_CAPACITY 16
 #define NAME_LEN 100
 #define PHONE_LEN 20
+#define SEARCH_ANY 0
+#define SEARCH_PHONE 1
+#define SEARCH_NAME 2
+
+int CASE = 0;
+int SEARCH = SEARCH_ANY;
 
 struct Contact {
     int id;
@@ -15,6 +21,7 @@ struct Contact {
 
 struct Contact *read_contacts(const char *filename, int *out_count) {
     FILE *file = fopen(filename, "r");
+    
     if (!file) {
         perror("Error reading file.");
         return NULL;
@@ -67,10 +74,46 @@ struct Contact *read_contacts(const char *filename, int *out_count) {
     return contacts;
 }
 
+int check_matches_name(const char *search_term, struct Contact contact) {
+    if (CASE) {
+        if (strstr(contact.name, search_term))
+            return 1;
+    } else {
+        if (strcasestr(contact.name, search_term))
+            return 1;
+    }
+    return 0;
+}
+
+int check_matches_phone(const char *search_term, struct Contact contact) {
+    if (strcasestr(contact.phone, search_term)) return 1;
+    return 0;
+}
+
+int check_matches_any(const char *search_term, struct Contact contact) {
+    if (CASE) {
+        if (strstr(contact.name, search_term))
+            return 1;
+        if (strstr(contact.phone, search_term))
+            return 1;
+    } else {
+        if (strcasestr(contact.name, search_term))
+            return 1;
+        if (strcasestr(contact.phone, search_term))
+            return 1;
+    }
+    return 0;
+}
+
 // returns 1 if matches, 0 otherwise
-int check_matches(const char *search_term, struct Contact contact) { 
-    if(strcasestr(contact.name, search_term)) return 1;
-    if(strcasestr(contact.phone, search_term)) return 1;
+int check_matches(const char *search_term, struct Contact contact) {
+    if (SEARCH == SEARCH_NAME) {
+        return check_matches_name(search_term, contact);
+    } else if (SEARCH == SEARCH_PHONE) {
+        return check_matches_phone(search_term, contact); 
+    } else {
+        return check_matches_any(search_term, contact);
+    }
     return 0;
 }
 
@@ -80,7 +123,7 @@ struct Contact *find_contacts(struct Contact *contacts, int contacts_size,
     struct Contact *matches = malloc(contacts_size * sizeof(struct Contact));
 
     for (int i = 0; i < contacts_size; i++) {
-        if(check_matches(search_term, contacts[i])) {
+        if (check_matches(search_term, contacts[i])) {
             matches[count] = contacts[i];
             count++;
         }
@@ -91,8 +134,23 @@ struct Contact *find_contacts(struct Contact *contacts, int contacts_size,
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        puts("Usage: phonefind <csv-file> <search-term>");
+        puts("Usage: phonefind <csv-file> <search-term> --case --search=name|phone");
         return 1;
+    }
+
+    if (argc > 3) {
+        for(int i = 3; i < argc; i++) {
+            if (strcmp(argv[i], "--case") == 0) {
+                CASE = 1;
+            } else if (strcmp(argv[i], "--search=name") == 0) {
+                SEARCH = SEARCH_NAME;
+            } else if (strcmp(argv[i], "--search=phone") == 0) {
+                SEARCH = SEARCH_PHONE;
+            } else {
+                puts("Usage: phonefind <csv-file> <search-term> --case --search=name|phone");
+                return 1;
+            }
+        }
     }
 
     char *filename = argv[1];
@@ -109,8 +167,8 @@ int main(int argc, char *argv[]) {
     int match_count = 0;
     struct Contact *matches =
         find_contacts(contacts, count, search_term, &match_count);
-    
-    if(!match_count) {
+
+    if (!match_count) {
         puts("No contacts found");
         return 0;
     }
