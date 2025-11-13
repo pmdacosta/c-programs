03109b3e992a648da371a1894e4d90ae39a7963f
Pedro Costa <pmdacosta@protonmail.com>
2025-11-13 18:36:51 +0000

    sdltut: ep 12

--- SDL2_Tutorials/6_OpenGL_SDL2/build.sh
+++ SDL2_Tutorials/6_OpenGL_SDL2/build.sh
@@ -1,6 +1,6 @@
-FLAGS='-Wall'
+FLAGS='-std=c89 -pedantic -Wall -Wextra'
 INCLUDE='-Iglad/include -I/usr/include/SDL2'
 LIBS='-lSDL2 -ldl'
 SRC='glad/src/glad.c main.c'
-OUT='main'
+OUT='prog'
 clang $FLAGS $INCLUDE $LIBS $SRC -o $OUT
--- SDL2_Tutorials/6_OpenGL_SDL2/main.c
+++ SDL2_Tutorials/6_OpenGL_SDL2/main.c
@@ -1,16 +1,79 @@
-// On linux compile with:
-// g++ -std=c++17 main.cpp glad/src/glad.c -I./glad/include -o prog -lSDL2 -ldl
-// On windows compile with (if using mingw)
-// g++ main.cpp ./glad/src/glad.c -I./glad/include -o prog.exe -lmingw32 -lSDL2main -lSDL2
-
-// C++ Standard Libraries
-// Third-party library
 #include "SDL.h"
-// Include GLAD
-#include <glad/glad.h>
+#include "glad/glad.h"
+#include <stdio.h>
 
-int main(int argc, char* argv[]){
-    SDL_Init(0);
+#define print_sdl_error(message) fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, message, SDL_GetError())
+
+int
+main(void)
+{
+    SDL_Window *window;
+    SDL_GLContext *context;
+    Uint8 running;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+    if (!window)
+    {
+        print_sdl_error("SDL_CreateWindow failed");
+        return 1;
+    }
+
+    context = SDL_GL_CreateContext(window);
+    if (!context)
+    {
+        print_sdl_error("SDL_GL_CreateContext failed");
+        return 1;
+    }
+
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+    glViewport(0,0, 640, 480);
+
+    running = 1;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+            {
+                running = 0;
+                goto quit;
+                break;
+            }
+            case SDL_MOUSEMOTION:
+            {
+                break;
+            }
+            case SDL_KEYDOWN:
+            {
+                break;
+            }
+            default:
+            {
+                break;
+            }
+            }
+        }
+
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+        SDL_Delay(16);
+    }
+
+quit:
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
